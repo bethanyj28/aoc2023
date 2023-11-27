1579c2996cae414eb99179aa07be4413192e35f8
bethanyj28 <bethanyjanos28@gmail.com>
2023-11-26 21:36:03 -0500

    scaffold with mix

---
+++ lib/aoc2023.ex
@@ -0,0 +1,6 @@
+defmodule Aoc2023 do
+  @moduledoc """
+    Module containing advent of code solutions. The aoc2023 file contains files per day. Each day has a `part1` and `part2` function. An example of how to run these is:
+      iex> Aoc2023.Day0.part1(0)
+  """
+end
---
+++ lib/aoc2023/day0.ex
@@ -0,0 +1,9 @@
+defmodule Aoc2023.Day0 do
+  def part1(args) do
+    IO.puts("hello, world: #{args}")
+  end 
+
+  def part2(args) do
+    IO.puts("hello, world: #{args}")
+  end
+end
