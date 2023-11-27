7e327a1b4e177b6fddf3fee1592f83523139b63f
bethanyj28 <bethanyjanos28@gmail.com>
2023-11-26 21:44:27 -0500

    scaffold tests

---
+++ test/aoc2023/day0_test.exs
@@ -0,0 +1,16 @@
+defmodule Aoc2023.Day0Test do
+  use ExUnit.Case
+  import Aoc2023.Day0
+
+  @tag :skip
+  test "part1" do
+    input = 20  
+    assert ExUnit.CaptureIO.capture_io(fn -> part1(input) end) == "hello, world: 20\n"
+  end
+
+  @tag :skip
+  test "part2" do
+    input = 20  
+    assert ExUnit.CaptureIO.capture_io(fn -> part2(input) end) == "hello, world: 20\n"
+  end
+end
